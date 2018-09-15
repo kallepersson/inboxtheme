f946f9e897fd9f9a660c88d24e256287dcdd4359
Kalle Persson <kalle@kallepersson.se>
2018-09-16 01:38:06 +0200

    Update header background based on selected tags

--- in.js
+++ in.js
@@ -3,16 +3,14 @@ _styleElement.innerText = _css;
 
 const init = (evt) => {
   document.head.appendChild(_styleElement);
-  console.log("ready", _styleElement.parentNode)
+  handleHashChange();
 }
 
 const handleHashChange = (evt) => {
-  console.log("#", window.location.hash);
   document.body.dataset.hash = window.location.hash;
 }
 
 window.addEventListener("hashchange", handleHashChange);
-
 
 if (document.head) {
   init();
